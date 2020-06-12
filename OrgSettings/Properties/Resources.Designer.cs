f9736a61e16d2a639c7726f51f13258dbf08b342
LinkeD365 <carl.cookson@gmail.com>
2020-06-12 15:57:31 +0100

    Tidied Reference 👊🏾

--- OrgSettings/Properties/Resources.Designer.cs
+++ OrgSettings/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OrgSettings.Properties {
-    using System;
-    
-    
+namespace OrgSettings.Properties
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
